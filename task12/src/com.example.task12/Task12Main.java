27ea415bffeaae2cbd9b678d193a5f10100e164f
Zakharov Danila <danila.zaxarov.2015@gmail.com>
2024-09-17 14:24:52 +0300

    task 12. Zakharov Danila

--- task12/src/com.example.task12/Task12Main.java
+++ task12/src/com.example.task12/Task12Main.java
@@ -1,5 +1,7 @@
 package com.example.task12;
 
+import java.util.Arrays;
+
 public class Task12Main {
     public static void main(String[] args) {
         //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
@@ -12,7 +14,31 @@ public class Task12Main {
     }
 
     static void selectionSort(int[] arr) {
-        //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
+        if (arr == null || arr.length < 2) {return;}
+        for (int i = 0; i < arr.length - 2; i++) {
+            int[] minAndIndex = findMinValueAndIndex(arr, i, arr.length - 1 );
+            arr[minAndIndex[1]] = arr[i];
+            arr[i] = minAndIndex[0];
+        }
     }
 
+
+    public static int[] findMinValueAndIndex(int[] array, int startIndex, int endIndex) {
+        if (startIndex < 0 || endIndex >= array.length || startIndex > endIndex) {
+            return null; // Промежуток некорректен
+        }
+
+        int minValue = array[startIndex];
+        int minIndex = startIndex;
+
+        for (int i = startIndex + 1; i <= endIndex; i++) {
+            if (array[i] < minValue) {
+                minValue = array[i];
+                minIndex = i;
+            }
+        }
+
+        return new int[]{minValue, minIndex};
+    }
 }
+
