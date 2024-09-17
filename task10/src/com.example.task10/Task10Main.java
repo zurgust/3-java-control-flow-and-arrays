f67b63c19cb7f5ef45e4c98a238263ddfc90d6ae
Zakharov Danila <danila.zaxarov.2015@gmail.com>
2024-09-17 13:34:32 +0300

    task 10. Zakharov Danila

--- task10/src/com.example.task10/Task10Main.java
+++ task10/src/com.example.task10/Task10Main.java
@@ -1,5 +1,8 @@
 package com.example.task10;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 public class Task10Main {
     public static void main(String[] args) {
         //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
@@ -12,7 +15,15 @@ public class Task10Main {
 
     static int numMin(int[] arr) {
         //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
-        return 0;
+        int min = Arrays.stream(arr).min().getAsInt();
+        int lastIndex = 0;
+        for (int i = arr.length - 1; i >= 0; i--) {
+            if (min == arr[i]) {
+                lastIndex = i;
+                break;
+            }
+        }
+        return lastIndex;
     }
 
 }
