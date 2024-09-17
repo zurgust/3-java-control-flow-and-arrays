2ae7ff5ca18475091a6c20a91ae1e44cf2e45f6b
Zakharov Danila <danila.zaxarov.2015@gmail.com>
2024-09-17 18:14:29 +0300

    task 13. Zakharov Danila

--- task13/src/com.example.task13/Task13Main.java
+++ task13/src/com.example.task13/Task13Main.java
@@ -4,16 +4,31 @@ public class Task13Main {
     public static void main(String[] args) {
         //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
         // например вот так:
-        /*
-        int[] arr = {9, 1100, 7, 8};
-        removeMoreThen1000(arr);
+        int[] arr = {};
+        arr = removeMoreThen1000(arr);
         System.out.println(java.util.Arrays.toString(arr));
-         */
     }
 
     static int[] removeMoreThen1000(int[] arr) {
-        //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
-        return  null;
+        int[] result = {};
+        if (arr == null ) return null;
+        else {
+            for (int j : arr) {
+                if (j <= 1000) {
+                    result = addElement(result, j);
+                }
+            }
+            return result;
+        }
+    }
+
+    public static int[] addElement(int[] array, int newElement) {
+
+        int[] newArray = new int[array.length + 1];
+        System.arraycopy(array, 0, newArray, 0, array.length);
+        newArray[array.length] = newElement;
+
+        return newArray;
     }
 
 }
